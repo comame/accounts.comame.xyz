772d8c23d5f416d6b69b8940e31d5d161dafe910
comame <dev@comame.xyz>
2022-10-11 22:06:21 +0900

    lint

--- src/http/set_header.rs
+++ src/http/set_header.rs
@@ -3,10 +3,10 @@ use hyper::{Request, Response};
 
 pub fn set_header<T>(res: &mut Response<T>, key: &'static str, value: &str) {
     let header_value = HeaderValue::from_str(value).unwrap();
-    res.headers_mut().append(key.clone(), header_value);
+    res.headers_mut().append(key, header_value);
 }
 
 pub fn set_header_req<T>(req: &mut Request<T>, key: &'static str, value: &str) {
     let header_value = HeaderValue::from_str(value).unwrap();
-    req.headers_mut().append(key.clone(), header_value);
+    req.headers_mut().append(key, header_value);
 }
